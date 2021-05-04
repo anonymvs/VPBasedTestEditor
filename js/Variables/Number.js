388a22411303625e53d1569d4e35703cd5bccb85
Fonyo Viktoria <viktoria.fonyo@techray.com>
2021-05-04 15:04:10 +0200

    variables

---
+++ js/Variables/Number.js
@@ -0,0 +1,55 @@
+var numSocket = new Rete.Socket('Number value 3');
+
+var VueNumControl = {
+  props: ['readonly', 'emitter', 'ikey', 'getData', 'putData'],
+  template: '<input type="number" :readonly="readonly" :value="value" @input="change($event)" @dblclick.stop="" @pointerdown.stop="" @pointermove.stop=""/>',
+  data() {
+    return {
+      value: 0,
+    }
+  },
+  methods: {
+    change(e){
+      this.value = +e.target.value;
+      this.update();
+    },
+    update() {
+      if (this.ikey)
+        this.putData(this.ikey, this.value)
+      this.emitter.trigger('process');
+    }
+  },
+  mounted() {
+    this.value = this.getData(this.ikey);
+  }
+}
+
+class NumControl extends Rete.Control {
+
+  constructor(emitter, key, readonly) {
+    super(key);
+    this.component = VueNumControl;
+    this.props = { emitter, ikey: key, readonly };
+  }
+
+  setValue(val) {
+    this.vueContext.value = val;
+  }
+}
+
+class NumComponent extends Rete.Component {
+
+    constructor(){
+        super("Number");
+    }
+
+    builder(node) {
+        var out1 = new Rete.Output('num', "Number", numSocket);
+
+        return node.addControl(new NumControl(this.editor, 'num')).addOutput(out1);
+    }
+
+    worker(node, inputs, outputs) {
+        outputs['num'] = node.data.num;
+    }
+}
