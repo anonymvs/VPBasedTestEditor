dfe022248c902b12d4791cabf5a0ae251bbbc6f9
Kristóf Kanyó <kanyokristof95@gmail.com>
2021-05-05 11:05:54 +0200

    Rete width es height beallitasa

--- js/main.js
+++ js/main.js
@@ -54,3 +54,8 @@ var editor;
 $('.no-href').click(function(e){
   e.preventDefault();
 });
+
+$( window ).resize(function() {
+  $('#rete').css ('width', '100hh');
+  $('#rete').css ('height', '100vh');
+});
