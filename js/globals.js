447d2dcbccbc7160a83c18c4cd0a45138f16de4a
Fonyo Viktoria <viktoria.fonyo@techray.com>
2021-05-05 13:49:00 +0200

    lemaradt

--- js/globals.js
+++ js/globals.js
@@ -1,3 +1,3 @@
 var editor;
 
-var components = [new NumComponent (), new StrComponent (), new Coord2DComponent (), new Coord3DComponent (), new SimpleComponent ()];
+var components = [new NumComponent (), new StrComponent (), new Coord2DComponent (), new Coord3DComponent (), new SimpleComponent (), new BeginComponent (), new EndComponent ()];
